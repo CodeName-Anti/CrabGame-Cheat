17a4abc5d28305eda7b927dbaeb3439e1bf60504
JNNJ <nikanerix@gmail.com>
2021-10-31 23:30:17 +0100

    Version Bump

--- CrabGame Cheat/Properties/AssemblyInfo.cs
+++ CrabGame Cheat/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using MelonLoader;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: MelonInfo(typeof(Cheat), "CrabGame Cheat", "1.2.1", "JNNJ")]
+[assembly: MelonInfo(typeof(Cheat), "CrabGame Cheat", "1.3", "JNNJ")]
 [assembly: MelonGame("Dani", "Crab Game")]
 [assembly: AssemblyTitle("CrabGame Cheat")]
 [assembly: AssemblyDescription("A cheat for the Game \"Crab Game\"")]
@@ -36,6 +36,6 @@ using MelonLoader;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
