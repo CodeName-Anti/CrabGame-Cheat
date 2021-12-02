936b801befaa7260586c962561022298a0976e36
CodeName-Anti <bamblampel@gmail.com>
2021-12-02 20:35:34 +0100

    - Removed ChatCommands

--- CrabGame Cheat/Util/CheatLog.cs
+++ CrabGame Cheat/Util/CheatLog.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx.Logging;
 using JNNJMods.CrabGameCheat.Loader;
+using JNNJMods.CrabGameCheat.Patches;
 using JNNJMods.CrabGameCheat.Translators;
 
 namespace JNNJMods.CrabGameCheat.Util
@@ -42,24 +43,5 @@ namespace JNNJMods.CrabGameCheat.Util
             logSource.LogWarning(message);
 
         }
-
-        /// <summary>
-        /// Logs a message to the Chatbox. <br></br>
-        /// <b>Should only be called InGame!</b>
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="displayUsername"></param>
-        public static void LogChatBox(object message, bool displayUsername = true)
-        {
-            var chat = ChatBox.Instance;
-
-            chat.messages.text += "\n" + (displayUsername ? "<color=#5100ff>CrabGame Cheat</color>:" : "") + $"{message}";
-            //Show chat
-            chat.Method_Private_Void_1();
-
-            chat.CancelInvoke("HideChat");
-            chat.Invoke("HideChat", 5f);
-        }
-
     }
 }
