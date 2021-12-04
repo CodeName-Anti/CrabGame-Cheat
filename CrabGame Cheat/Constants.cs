8153f6b8d36796053b7f7248c1d14a86136963cb
CodeName-Anti <bamblampel@gmail.com>
2021-12-04 17:15:29 +0100

    - Bump Version

--- CrabGame Cheat/Constants.cs
+++ CrabGame Cheat/Constants.cs
@@ -4,7 +4,7 @@
     {
         // Assembly and Plugin related
         public const string
-            Version = "1.6.7.9",
+            Version = "1.6.7.10",
             GUID = "de.jnnj.crabgamecheat";
 
         // GitHub related
