61cd75d7819afb503c0ac570bb6fe0a213cbedae
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2025-01-05 15:32:00 +0100

    Remove unecessary constant

--- CrabCheat/Constants.cs
+++ CrabCheat/Constants.cs
@@ -6,6 +6,5 @@ public static class Constants
 	public const string
 		Repository = "CrabGame-Cheat",
 		Owner = "CodeName-Anti",
-		DownloadURL = $"https://github.com/{Owner}/{Repository}/releases/latest/download/CrabCheat_BepInEx.dll",
 		ReleasesAPI = $"https://api.github.com/repos/{Owner}/{Repository}/releases";
 }
