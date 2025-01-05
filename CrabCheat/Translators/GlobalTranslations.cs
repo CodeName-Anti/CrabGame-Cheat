57f656726698d94de132e43554537ef570779492
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2025-01-05 15:39:13 +0100

    Reformat GlobalTranslations + Remove namespace

--- CrabCheat/Translators/GlobalTranslations.cs
+++ CrabCheat/Translators/GlobalTranslations.cs
@@ -2,28 +2,31 @@
 global using GameModeBombTag = GameModePublicInSiLiSiIn1UIObBoUnique;
 global using GameModeHat = GameModePublicLi1UItaDi2InUIplSiUnique;
 global using GameModeTag = GameModePublicLi1UIUnique;
+
 // GameModes
 global using GameModeRedLight = GameModePublicSiBoDi2UIObmoSiroSiUnique;
 global using PersistentPlayerData = MonoBehaviourPublicBofrhnBoObInUnique;
+
 // Online Player
 global using PlayerManager = MonoBehaviourPublicCSstReshTrheObplBojuUnique;
 global using ItemManager = MonoBehaviourPublicDi2InItidGamoObInUnique;
 global using GameManager = MonoBehaviourPublicDi2UIObacspDi2UIObUnique;
 global using PlayerMovement = MonoBehaviourPublicGaplfoGaTrorplTrRiBoUnique;
 global using Prompt = MonoBehaviourPublicGaprLi1ObGaprInUnique;
+
 // Server
 global using ServerSend = MonoBehaviourPublicInInUnique;
+
 // Client
 global using ClientSend = MonoBehaviourPublicInpabyInInInUnique;
 global using QuestManager = MonoBehaviourPublicLi1QudaDi2InquQuacUnique;
 global using PlayerStatus = MonoBehaviourPublicObcumaObInplInObUnique;
 global using GameLoop = MonoBehaviourPublicObInLi1GagasmLi1GaUnique;
-global using MapManager = MonoBehaviourPublicObInMamaLi1plMadeMaUnique; // Probably has an other name, as it isnt in the pre-obfuscated version
+global using MapManager = MonoBehaviourPublicObInMamaLi1plMadeMaUnique; // Probably has an other name, as it isn't in the pre-obfuscated version
 global using SteamManager = MonoBehaviourPublicObInUIgaStCSBoStcuCSUnique;
 global using OnlinePlayerMovement = MonoBehaviourPublicObVeSiVeRiSiAnVeanTrUnique;
+
 // UI
 global using ChatBox = MonoBehaviourPublicRaovTMinTemeColoonCoUnique;
 global using CameraShaker = MonoBehaviourPublicShdaguShpuheObShInShUnique;
 global using VersionUI = MonoBehaviourPublicTeveObInUnique;
-
-namespace JNNJMods.CrabCheat.Translators;
